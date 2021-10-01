405a46701a893298296a7e1ff5e2a40d6ba67ef9
bhatt098 <55306347+bhatt098@users.noreply.github.com>
2021-10-01 10:55:21 +0530

    Update dbcontroller.php

--- includes/dbcontroller.php
+++ includes/dbcontroller.php
@@ -1,8 +1,8 @@
 <?php
-$DB_host = "localhost";
-$DB_user = "root";
-$DB_pass = "";
-$DB_name = "hostel";
+$DB_host = "remotemysql.com";
+$DB_user = "lWtDeLM9xM";
+$DB_pass = "tUNZfeby4Z";
+$DB_name = "lWtDeLM9xM";
 try
 {
  $DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
